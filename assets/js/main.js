6fc7b01546b3a1944e6f10f10d4f40c1fcb2c2ee
asadbek <asadbekqulboyev@gmail.com>
2025-02-08 17:48:20 +0500

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,47 @@
 $(document).ready(function () {
-    var swiper = new Swiper('.swiper')
+    var swiperLeft = new Swiper('.left_content .swiper',{
+        navigation: {
+            nextEl: '.left_content .swiper-button-next',
+            prevEl: '.left_content .swiper-button-prev' 
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true, 
+        },
+        autoplay: {
+            delay: 3000
+        },
+        loop:true
+    })
+    // sidebar links
+    $('.menu_header').click(function () {
+        var $child = $(this).next('.menu_child');
+    
+        if ($child.is(':visible')) {
+            $child.slideUp(); 
+        } else {
+            $('.menu_child').slideUp();
+            $child.slideDown(); 
+        }
+    });
+    // slide thumnail
+    var swiper = new Swiper(".right_content .mySwiper", {
+        loop: true,
+        spaceBetween: 0,
+        slidesPerView: 5,
+        freeMode: true,
+        watchSlidesProgress: true,
+      });
+    //   slide pagination img
+    var swiper2 = new Swiper(".right_content .mySwiper2", {
+        loop: true,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".right_content .swiper-button-next",
+          prevEl: ".right_content .swiper-button-prev",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+    });
 })
